bf0a10de2d37967f20d828618bc810489b13916a
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-27 06:41:08 +0200

    init project from project 14.8 - git searching

---
+++ components/search.js
@@ -0,0 +1,39 @@
+Search = React.createClass({
+  getInitialState() {
+      return {
+        searchingTerm: ''
+      }
+  },
+  handleChange: function(event) {
+    var searchingText = event.target.value;
+    this.setState({
+      searchingText: searchingText
+    });
+    if (searchingText.lenght > 2) {
+      this.props.onSearch(searchingText);
+    }
+  },
+
+  handleKeyUp: function(event) {
+    if (event.keyCode === 13) {
+      this.props.onSearch(this.state.searchingText);
+    }
+  },
+
+  render: function() {
+    var styles = {
+      fontSize: '1.5em',
+      width: '90%',
+      maxWidth: '350px'
+    };
+
+    return <input
+            type='text'
+            onChange={this.handleChange}
+            onKeyUp={this.handleKeyUp}
+            placeholder="write down what are you looking for"
+            style={styles}
+            value={this.state.searchTerm}
+            />
+  }
+})
